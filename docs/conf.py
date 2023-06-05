5ee6aac598701491086bc30825da8d73326e35bc
sschrunner <stefan.schrunner@protonmail.com>
2023-06-05 18:51:46 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -55,4 +55,4 @@ html_theme = 'alabaster'
 # so a file named "default.css" will overwrite the builtin "default.css".
 # html_static_path = ['_static']
 
-autodoc_mock_imports = ["numpy","pandas","scikit-learn","scipy","random", "sklearn-features", "mrmr", "pygad"]
+autodoc_mock_imports = ["numpy","pandas","sklearn","scipy","random","skfeatures", "mrmr", "pygad"]
