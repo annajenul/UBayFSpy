78883bc0e3e2f4f0e431fbbd63065e031fe7fef7
Stefan Schrunner <stefan.schrunner@nmbu.no>
2023-06-05 17:30:10 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -42,6 +42,8 @@ master_doc = "index"
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
+project.plugin_depends_on("numpy")
+
 
 # -- Options for HTML output -------------------------------------------------
 
