9ece02bac499678500a4cd086e5ddd23ab151af7
annajenul <anna.jenul@gmail.com>
2023-06-30 10:12:31 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -15,6 +15,7 @@ import sys
 sys.path.insert(0, os.path.abspath('../src/UBayFS/'))
 
 
+
 # -- Project information -----------------------------------------------------
 
 project = 'UBayFS'
@@ -30,7 +31,7 @@ release = '1.0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon', 'sphinx.ext.mathjax']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon', 'sphinx.ext.mathjax', 'nbsphinx']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
